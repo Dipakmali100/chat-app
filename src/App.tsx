3a6db3b24065b9a88893ba295cb1032dfc08879c
Dipak Mali <malidipakb2002@gmail.com>
2024-09-27 01:50:52 +0530

    Initialized the Frontend

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center'>
+        <div className='border-black border-2 w-1/4 text-center bg-blue-300'>
+          Real-Time Chat Application
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
